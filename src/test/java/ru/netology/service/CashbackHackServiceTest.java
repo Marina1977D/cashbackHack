8e2013ebbd57b1ab3071065d2ba7d82d16a98b0a
Marina1977D <deevamariha@gmail.com>
2022-09-18 11:46:47 +0300

    junit tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
+import org.junit.Assert;
+import org.junit.Test;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
